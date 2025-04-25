b40cb5d2e836a7cf97bd03ecd7b1497d3b43df08
akankshamahto1301 <akankshamahto1301@gmail.com>
2025-04-25 12:27:59 +0530

    prep for deployment

---
+++ app/waitlist/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const WaitlistPage = () => {
+  return (
+    <div className="mt-20 fixed w-full">
+      <div className="aspect-video w-full">
+        <iframe
+          style={{ width: "100%" }}
+          height="569"
+          src="https://www.create.xyz/app/c4a49e08-ad28-4714-84cd-70c0be09a6e8"
+          title="Waiting List Landing Page"
+          frameBorder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default WaitlistPage;
+
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsHmrCache: false,
+    },
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "vrqgsklnnmjzeltihtqw.supabase.co",
+            }
+        ]
+    },
+
+    async headers() {
+        return [
+            {
+                source: "/embed",
+                headers:[
+                    {
+                     key:"Content-Security-Policy",
+                     value:"frame-src 'self' https://www.create.xyz/app/c4a49e08-ad28-4714-84cd-70c0be09a6e8;"
+                    },
+
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
